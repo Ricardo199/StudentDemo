0089c14d903c78e374dc705b4bb7b2d0b2ecb95b
Ricardo199 <ricardo1529@gmail.com>
2024-05-13 15:58:05 -0400

    i made a bunch of changes but i didn't commit, just push, my bad

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,7 +11,13 @@ namespace StudentDemo
     {
         static void Main()
         {
-
+            Student student= new Student();
+            Console.WriteLine("Please enter your name ");
+            student.Name= Console.ReadLine();
+            Console.WriteLine("Please enter your id");
+            student.Id= int.Parse(Console.ReadLine());
+            student.SayHi();
+            Console.ReadLine();
         }
     }
 }
---
+++ Student.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentDemo
+{
+    internal class Student
+    {
+        private int id;
+        private string name;
+
+        public int Id { 
+            get { return id; }
+            set { id = value; }
+        }
+        public string Name { 
+            get { return name; }
+            set { name = value; }
+        }
+
+        public void SayHi() {
+            Console.WriteLine($"Hi, my name is: {Name}, and my id is: {Id}.");
+        }
+
+    }
+}
